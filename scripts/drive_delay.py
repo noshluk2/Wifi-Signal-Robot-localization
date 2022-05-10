bd02c83aa0901d2ebeb4064d02ea5ed39d4fd603
noshluk2 <noshluk2@gmail.com>
2022-05-10 16:21:43 +0500

    network data integeration

--- scripts/drive_delay.py
+++ scripts/drive_delay.py
@@ -1,15 +1,19 @@
 import time
 from tracemalloc import stop
-
+import networkdata as nd
 import utilities as ut
 
 time.sleep(3)
 
 
 motors = ut.motor_setup(23,24,25,14,15,4)
-
+nd_obj=nd.Network_data_to_excel("luqman.xls")
+nd_obj.open_sheet()
 
 drive_flag=True
+
+
+
 
 def motion(distance):
     motors.forward()
@@ -27,15 +31,27 @@ def turn_90():
 while(drive_flag):
     print("Turning robot ON ! ")
     time.sleep(3)
+    nd_obj.data_append()
+    nd_obj.line_skip()
+    nd_obj.data_append()
 
     motion(2)
     turn_90()
+    nd_obj.data_append()
+    nd_obj.line_skip()
+    nd_obj.data_append()
+
 
     motion(1)
     turn_90()
+    nd_obj.data_append()
+    nd_obj.line_skip()
+    nd_obj.data_append()
+
 
     drive_flag=False
     motors.motor_turn_off()
+    nd_obj.save_book()
 
 
 
